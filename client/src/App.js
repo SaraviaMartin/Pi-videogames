7e5342ef68be78abf2c08c4dd90447508a573b90
Juan Saravia <saraviamartin16@hotmail.com>
2023-05-12 01:45:06 -0300

    comence el front, components y views

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
+import Home from './views/Home/Home';
+// import Landing from './Views/LandingPage/Landing';
+// import Form from './Views/Form/Form';
+// import Detail from './Views/Detail/Detail';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Routes>
+          <Switch>
+          <Route  path='/home' element={ <Home /> }></Route>
+          </Switch>
+          
+          
+        </Routes>
+    
+      </div>
+    );
 }
 
 export default App;
